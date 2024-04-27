eade1f6662e889311e16307f915c290d334d6fdd
Ainur Shamsutdinov <ainurshamsutdinov@Ainurs-MacBook-Pro.local>
2024-04-28 01:11:36 +0300

    4 task solution

--- seminar/sem_app/models.py
+++ seminar/sem_app/models.py
@@ -21,3 +21,31 @@ class TossCoin(models.Model):
 
     def __str__(self):
         return f'Result: {self.result}, Date: {self.date}'
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    full_name = models.CharField(max_length=60)
+    email = models.EmailField()
+    biography = models.TextField()
+    birthday = models.DateField()
+
+    def __init__(self, name, last_name, email, biography, birthday, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = name
+        self.last_name = last_name
+        self.full_name = f'{name} {last_name}'
+        self.email = email
+        self.biography = biography
+        self.birthday = birthday
+
+
+class Article(models.Model):
+    head = models.CharField(max_length=200)
+    content = models.TextField()
+    publish_date = models.DateField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100)
+    views = models.IntegerField(default=0)
+    published = models.BooleanField(default=False)
